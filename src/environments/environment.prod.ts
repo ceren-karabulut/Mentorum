2b0a7e1169ada6b4988f820f7adcecabcac9e235
ceren-karabulut <cerenkarabuluttt@gmail.com>
2021-03-06 04:16:48 +0300

    android build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCU3BuMQ1yFbDKXdms4Iy-GV-sNe62wa4Q",
+    authDomain: "mentorum-fd84c.firebaseapp.com",
+    projectId: "mentorum-fd84c",
+    storageBucket: "mentorum-fd84c.appspot.com",
+    messagingSenderId: "110061954638",
+    appId: "1:110061954638:web:634d3ff85f3eb24b28451f",
+    measurementId: "G-MVE0SK7Y1B"
+  }
 };
